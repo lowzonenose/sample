c7dae623f947483164ee758eb4ac1c39eb9d9747
jpbazonnais <jean-philippe.bazonnais@ign.fr>
2015-08-07 15:26:08 +0200

    update jsdoc

---
+++ jsdoc/config/i18N/fr_FR/loader-extend.js
@@ -0,0 +1,10 @@
+{
+    "<p>Exemple d'implementation d'un heritage en JS avec RequireJS avec les instructions JSDoc.</p>\n<p>Référence sur l'implementation d'une classe en JS :\ncf. <a href=\"http://code-weblog.com/programmation-orientee-objet-en-javascript/\">http://code-weblog.com/</a></p>": "<p>Exemple d'implementation d'un heritage en JS avec RequireJS avec les instructions JSDoc.</p>\n<p>Référence sur l'implementation d'une classe en JS :\ncf. <a href=\"http://code-weblog.com/programmation-orientee-objet-en-javascript/\">http://code-weblog.com/</a></p>",
+    "<p>Exemple de constructeur (fonction)</p>": "<p>Exemple de constructeur (fonction)</p>",
+    "<p>heritage des comportements de la classe mère</p>": "<p>heritage des comportements de la classe mère</p>",
+    "<p>Exemple de constructeur (alias)</p>": "<p>Exemple de constructeur (alias)</p>",
+    "<p>Exemple de surcharge d'un comportement de la classe mère</p>": "<p>Exemple de surcharge d'un comportement de la classe mère</p>",
+    "<p>Exemple d'ajout d'un comportement spécifique à la classe fille</p>": "<p>Exemple d'ajout d'un comportement spécifique à la classe fille</p>",
+    "<p>Méthodes d'instance\nErreur de chargement d'un script</p>": "<p>Méthodes d'instance\nErreur de chargement d'un script</p>",
+    "<p>Méthodes d'instance\nSucces du chargement d'un script</p>": "<p>Méthodes d'instance\nSucces du chargement d'un script</p>"
+}
---
+++ jsdoc/config/i18N/fr_FR/loader.js
@@ -0,0 +1,18 @@
+{
+    "<p>Exemple d'implementation du classe JS avec RequireJS avec les instructions JSDoc.</p>\n<p>Référence sur l'implementation d'une classe en JS :\ncf. <a href=\"http://code-weblog.com/programmation-orientee-objet-en-javascript/\">http://code-weblog.com/</a></p>": "<p>Exemple d'implementation du classe JS avec RequireJS avec les instructions JSDoc.</p>\n<p>Référence sur l'implementation d'une classe en JS :\ncf. <a href=\"http://code-weblog.com/programmation-orientee-objet-en-javascript/\">http://code-weblog.com/</a></p>",
+    "<p>Exemple de constructeur (fonction)</p>": "<p>Exemple de constructeur (fonction)</p>",
+    "<p>function callback success</p>": "<p>function callback success</p>",
+    "<p>function callback error</p>": "<p>function callback error</p>",
+    "<p>before or head (true)/after or body (false)</p>": "<p>before or head (true)/after or body (false)</p>",
+    "<p>true/false</p>": "<p>true/false</p>",
+    "<p>Description</p>": "<p>Description</p>",
+    "<p>Exemple d'attribut de la classe.</p>": "<p>Exemple d'attribut de la classe.</p>",
+    "<p>Exemple de méthodes de la classe (static).</p>": "<p>Exemple de méthodes de la classe (static).</p>",
+    "<p>Settings par defaut</p>": "<p>Settings par defaut</p>",
+    "<p>Exemple de méthodes de la classe (private)</p>": "<p>Exemple de méthodes de la classe (private)</p>",
+    "<p>Exemple de constructeur (alias)</p>": "<p>Exemple de constructeur (alias)</p>",
+    "<p>Exemple de méthodes d'instance (public)</p>": "<p>Exemple de méthodes d'instance (public)</p>",
+    "<p>Exemple de méthodes d'instance (private)</p>\n<p>Référence sur l'implementation 'private or static method' : \ncf. <a href=\"http://philipwalton.com/articles/implementing-private-and-protected-members-in-javascript/\">http://philipwalton.com/</a></p>": "<p>Exemple de méthodes d'instance (private)</p>\n<p>Référence sur l'implementation 'private or static method' : \ncf. <a href=\"http://philipwalton.com/articles/implementing-private-and-protected-members-in-javascript/\">http://philipwalton.com/</a></p>",
+    "<p>Méthodes d'instance\nErreur de chargement d'un script</p>": "<p>Méthodes d'instance\nErreur de chargement d'un script</p>",
+    "<p>Méthodes d'instance\nSucces du chargement d'un script</p>": "<p>Méthodes d'instance\nSucces du chargement d'un script</p>"
+}
